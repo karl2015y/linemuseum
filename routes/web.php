905638f342671a567aedeb1c7ddfb3644b03166b
karl <karl2015y@gmail.com>
2021-07-28 13:28:43 +0000

    改變Route的顯示方式

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\MuseumController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,20 +23,45 @@ Route::get('/', function () {
 
 // 總管後台 - 總管人員模組
 Route::prefix('admin')->group(function () {
-    // a0001 總管人員頁
-    Route::get('staffs', [StaffController::class, 'StaffsPage'])->name('StaffsPage');
-    // a0002 新增總管人員頁
-    Route::get('staffs/create', [StaffController::class, 'CreateStaffPage'])->name('CreateStaffPage');
-    // a0003 新增總管人員
-    Route::post('staffs/create', [StaffController::class, 'CreateStaff'])->name('CreateStaff');
-    // a0004 總管人員單頁
-    Route::get('staffs/{staff_id}', [StaffController::class, 'StaffPage'])->name('StaffPage');
-    // a0005 停用總管人員
-    Route::put('staffs/{staff_id}/disable',  [StaffController::class, 'DisableStaff'])->name('DisableStaff');
-    // a0006 編輯總管人員頁
-    Route::get('staffs/{staff_id}/edit', [StaffController::class, 'EditStaffPage'])->name('EditStaffPage');
-    // a0007 修改總管人員資料
-    Route::put('staffs/{staff_id}/edit',  [StaffController::class, 'EditStaff'])->name('EditStaff');
+    // StaffController
+    Route::prefix('staffs')->group(function () {
+        // a0001 總管人員頁
+        Route::get('/', [StaffController::class, 'StaffsPage'])->name('StaffsPage');
+        // a0002 新增總管人員頁
+        Route::get('/create', [StaffController::class, 'CreateStaffPage'])->name('CreateStaffPage');
+        // a0003 新增總管人員
+        Route::post('/create', [StaffController::class, 'CreateStaff'])->name('CreateStaff');
+        // a0004 總管人員單頁
+        Route::get('/{staff_id}', [StaffController::class, 'StaffPage'])->name('StaffPage');
+        // a0005 停用總管人員
+        Route::put('/{staff_id}/disable',  [StaffController::class, 'DisableStaff'])->name('DisableStaff');
+        // a0006 編輯總管人員頁
+        Route::get('/{staff_id}/edit', [StaffController::class, 'EditStaffPage'])->name('EditStaffPage');
+        // a0007 修改總管人員資料
+        Route::put('/{staff_id}/edit',  [StaffController::class, 'EditStaff'])->name('EditStaff');
+    });
+
+
+
+    // MuseumController
+    Route::prefix('museums')->group(function () {
+        // e0001 館舍列表頁
+        Route::get('/', [MuseumController::class, 'MuseumsPage'])->name('MuseumsPage');
+        // e0002 新增館舍頁
+        Route::get('/create', [MuseumController::class, 'CreateMuseumPage'])->name('CreateMuseumPage');
+        // e0003 新增館舍
+        Route::post('/create', [MuseumController::class, 'CreateMuseum'])->name('CreateMuseum');
+        // e0004 館舍單頁
+        Route::get('/{museum_id}', [MuseumController::class, 'MuseumPage'])->name('MuseumPage');
+        // e0005 封存館舍
+        Route::put('/{museum_id}/disable',  [MuseumController::class, 'DisableMuseum'])->name('DisableMuseum');
+        // e0006 刪除館舍
+        Route::get('/{museum_id}/delete', [MuseumController::class, 'DeleteMuseum'])->name('DeleteMuseum');
+        // e0007 編輯館舍頁
+        Route::get('/{museum_id}/edit', [MuseumController::class, 'EditMuseumPage'])->name('EditMuseumPage');
+        // e0008 編輯館舍
+        Route::put('/{museum_id}/edit',  [MuseumController::class, 'EditMuseum'])->name('EditMuseum');
+    });
 
 });
 
