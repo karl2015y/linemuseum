7639aa4c78ac1632f1277644d67d33415cb4c76b
karl <karl2015y@gmail.com>
2021-08-28 16:57:24 +0000

    更改初始化設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -75,124 +75,124 @@ class DatabaseSeeder extends Seeder
             'created_staff_id'=>$user->id,
             'updated_staff_id'=>$user->id,
         ]);
-        echo 'Create PayPointRecord data'. PHP_EOL;
-        \App\Models\PayPointRecord::factory()->count(30)->create([
-            'shop_id'=>$shop->id,
-            'shop_name'=>$shop->name,
-            'member_id'=>$test_user->id,
-            'member_name'=>$test_user->name,
-        ]);
-
-        echo 'Create KnowledgeActivity data'. PHP_EOL;
-        $KA = \App\Models\KnowledgeActivity::factory()->create([
-            'name'=>'好玩的活動',
-            'museum_id'=>1,
-            'status'=>'enable',
-            'start_at'=>Carbon::now(),
-            'end_at'=>Carbon::now()->addWeeks(1),
-            'point_cycle_hour'=>1,
-            'point_cycle_min'=>1,
-            'created_staff_id'=>$user->id,
-            'updated_staff_id'=>$user->id,
-        ]);
-        \App\Models\KnowledgeActivity::factory()->make([
-            'name'=>'未開始的活動',
-            'museum_id'=>1,
-            'status'=>'enable',
-            'start_at'=>Carbon::now()->addWeeks(1),
-            'end_at'=>Carbon::now()->addWeeks(2),
-            'created_staff_id'=>$user->id,
-            'updated_staff_id'=>$user->id,
-        ])->save();
-        \App\Models\KnowledgeActivity::factory()->make([
-            'name'=>'已結束的活動',
-            'museum_id'=>1,
-            'status'=>'enable',
-            'start_at'=>Carbon::now()->subWeeks(2),
-            'end_at'=>Carbon::now()->subWeeks(1),
-            'created_staff_id'=>$user->id,
-            'updated_staff_id'=>$user->id,
-        ])->save();
-
-        // echo 'Create KnowledgePointRecord data'. PHP_EOL;
-        // \App\Models\KnowledgePointRecord::factory()->count(30)->create([
-        //     'knowledge_activity_id' => $KA->id,
-        //     'knowledge_activity_name' => $KA->name,
-        //     'member_id' => $test_user->id,
-        //     'member_name' => $test_user->name,
+        // echo 'Create PayPointRecord data'. PHP_EOL;
+        // \App\Models\PayPointRecord::factory()->count(30)->create([
+        //     'shop_id'=>$shop->id,
+        //     'shop_name'=>$shop->name,
+        //     'member_id'=>$test_user->id,
+        //     'member_name'=>$test_user->name,
         // ]);
 
-        echo 'Create Voucher data'. PHP_EOL;
-        $vc = \App\Models\Voucher::factory()->create([
-            'name'=>'測試用兌換券',
-            'status'=>'enable',
-            'amount'=>1,
-            'created_staff_id'=>$user->id,
-            'updated_staff_id'=>$user->id,
-        ]);
+        // echo 'Create KnowledgeActivity data'. PHP_EOL;
+        // $KA = \App\Models\KnowledgeActivity::factory()->create([
+        //     'name'=>'好玩的活動',
+        //     'museum_id'=>1,
+        //     'status'=>'enable',
+        //     'start_at'=>Carbon::now(),
+        //     'end_at'=>Carbon::now()->addWeeks(1),
+        //     'point_cycle_hour'=>1,
+        //     'point_cycle_min'=>1,
+        //     'created_staff_id'=>$user->id,
+        //     'updated_staff_id'=>$user->id,
+        // ]);
+        // \App\Models\KnowledgeActivity::factory()->make([
+        //     'name'=>'未開始的活動',
+        //     'museum_id'=>1,
+        //     'status'=>'enable',
+        //     'start_at'=>Carbon::now()->addWeeks(1),
+        //     'end_at'=>Carbon::now()->addWeeks(2),
+        //     'created_staff_id'=>$user->id,
+        //     'updated_staff_id'=>$user->id,
+        // ])->save();
+        // \App\Models\KnowledgeActivity::factory()->make([
+        //     'name'=>'已結束的活動',
+        //     'museum_id'=>1,
+        //     'status'=>'enable',
+        //     'start_at'=>Carbon::now()->subWeeks(2),
+        //     'end_at'=>Carbon::now()->subWeeks(1),
+        //     'created_staff_id'=>$user->id,
+        //     'updated_staff_id'=>$user->id,
+        // ])->save();
 
-        echo 'Create MemberUsedVoucherRecord data'. PHP_EOL;
-        $VCRs = \App\Models\MemberUsedVoucherRecord::factory()->count(10)->create([
-            'voucher_id'=>$vc->id,
-            'voucher_name'=>$vc->name,
-            'member_id'=>$test_user->id,
-            'member_name'=>$test_user->name,
-        ]);
-        foreach ($VCRs as $vcr) {
-            \App\Models\VoucherRecordStatus::factory()->count(3)->create([
-                'voucher_record_id' => $vcr->id
-            ]);
-        }
+        // // echo 'Create KnowledgePointRecord data'. PHP_EOL;
+        // // \App\Models\KnowledgePointRecord::factory()->count(30)->create([
+        // //     'knowledge_activity_id' => $KA->id,
+        // //     'knowledge_activity_name' => $KA->name,
+        // //     'member_id' => $test_user->id,
+        // //     'member_name' => $test_user->name,
+        // // ]);
 
-        echo 'Create others data'. PHP_EOL;
-        foreach ($users as $user) {
-            if($user->id<=10){
-                \App\Models\Staff::factory()->setUserData($user->id, $user->email)->create();
-            }else if($user->id<=20){
-                \App\Models\Museum::factory()->setUserData($user->id, $user->email)->create();
-            }else if($user->id<=30){
-                \App\Models\Shop::factory()->setUserData($user->id, $user->email)->create([
-                    'museum_id'=>1,
-                ]);
-            }else if($user->id<=40){
-                \App\Models\Member::factory()->setUserData($user->id, $user->email)->create();
-            }
-        }
+        // echo 'Create Voucher data'. PHP_EOL;
+        // $vc = \App\Models\Voucher::factory()->create([
+        //     'name'=>'測試用兌換券',
+        //     'status'=>'enable',
+        //     'amount'=>1,
+        //     'created_staff_id'=>$user->id,
+        //     'updated_staff_id'=>$user->id,
+        // ]);
+
+        // echo 'Create MemberUsedVoucherRecord data'. PHP_EOL;
+        // $VCRs = \App\Models\MemberUsedVoucherRecord::factory()->count(10)->create([
+        //     'voucher_id'=>$vc->id,
+        //     'voucher_name'=>$vc->name,
+        //     'member_id'=>$test_user->id,
+        //     'member_name'=>$test_user->name,
+        // ]);
+        // foreach ($VCRs as $vcr) {
+        //     \App\Models\VoucherRecordStatus::factory()->count(3)->create([
+        //         'voucher_record_id' => $vcr->id
+        //     ]);
+        // }
+
+        // echo 'Create others data'. PHP_EOL;
+        // foreach ($users as $user) {
+        //     if($user->id<=10){
+        //         \App\Models\Staff::factory()->setUserData($user->id, $user->email)->create();
+        //     }else if($user->id<=20){
+        //         \App\Models\Museum::factory()->setUserData($user->id, $user->email)->create();
+        //     }else if($user->id<=30){
+        //         \App\Models\Shop::factory()->setUserData($user->id, $user->email)->create([
+        //             'museum_id'=>1,
+        //         ]);
+        //     }else if($user->id<=40){
+        //         \App\Models\Member::factory()->setUserData($user->id, $user->email)->create();
+        //     }
+        // }
 
 
 
 
-        echo 'Create others KnowledgeActivity data'. PHP_EOL;
-        \App\Models\KnowledgeActivity::factory()->count(10)->create([
-            'museum_id'=>1,
-        ]);
-        echo 'Create others Voucher data'. PHP_EOL;
-        \App\Models\Voucher::factory()->count(3)->create();
-        echo 'Create others VoucherWay data'. PHP_EOL;
-        \App\Models\VoucherWay::factory()->count(10)->create();
-        echo 'Create others KnowledgePointRecord data'. PHP_EOL;
-        \App\Models\KnowledgePointRecord::factory()->count(10)->create();
+        // echo 'Create others KnowledgeActivity data'. PHP_EOL;
+        // \App\Models\KnowledgeActivity::factory()->count(10)->create([
+        //     'museum_id'=>1,
+        // ]);
+        // echo 'Create others Voucher data'. PHP_EOL;
+        // \App\Models\Voucher::factory()->count(3)->create();
+        // echo 'Create others VoucherWay data'. PHP_EOL;
+        // \App\Models\VoucherWay::factory()->count(10)->create();
+        // echo 'Create others KnowledgePointRecord data'. PHP_EOL;
+        // \App\Models\KnowledgePointRecord::factory()->count(10)->create();
 
 
 
 
         
 
-        foreach (\App\Models\KnowledgeActivity::all() as $ka) {
-            $ka->Qrcode()->create();
-        }
+        // foreach (\App\Models\KnowledgeActivity::all() as $ka) {
+        //     $ka->Qrcode()->create();
+        // }
 
-        foreach (\App\Models\PayPointRecord::all() as $ppr) {
-            $ppr->Qrcode()->create();
-        }
+        // foreach (\App\Models\PayPointRecord::all() as $ppr) {
+        //     $ppr->Qrcode()->create();
+        // }
 
 
-        \App\Models\PayPointRecord::factory()->create([
-            'shop_id'=>$shop->id,
-            'shop_name'=>$shop->name,
-            'member_id'=>null,
-            'member_name'=>null,
-        ])->Qrcode()->create();
+        // \App\Models\PayPointRecord::factory()->create([
+        //     'shop_id'=>$shop->id,
+        //     'shop_name'=>$shop->name,
+        //     'member_id'=>null,
+        //     'member_name'=>null,
+        // ])->Qrcode()->create();
 
     }
 }
