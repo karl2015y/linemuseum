c6fcd373df1fc3a5f174aa79e2e0491b5c6accc0
karl <karl2015y@gmail.com>
2021-09-01 09:02:51 +0000

    https 改為動態

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+        // Url::forceScheme('https');
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
