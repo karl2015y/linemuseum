78bd08431d1f5f7aae7bfefc7dd28f332319c16f
karl <karl2015y@gmail.com>
2021-09-01 11:29:37 +0000

    新增廠商的忘記密碼說明

--- resources/views/phone/museum/LoginPage.blade.php
+++ resources/views/phone/museum/LoginPage.blade.php
@@ -12,10 +12,26 @@
     </div>
     <input value="{{ old('email') }}" name="email" class=" shadow-inner mt-1 mb-10 w-full bg-gray-100 p-2.5 rounded" type="text" placeholder="館舍登記電子信箱">
     <input name="password" class="shadow-inner mb-1 w-full bg-gray-100 p-2.5 rounded" type="password" placeholder="密碼">
+    <div class="flex justify-end items-center pr-1.5 text-gray-400 ">
+        <a href="javascript:forgotpassword();" >忘記密碼？</a>
+    </div>
     <div class="mt-6 mb-10">
         <button
             class="w-full py-3 px-6 text-white rounded bg-color-main shadow block md:inline-block">登入</button>
     </div>
 
 </form>
+@stop
+
+@section('js-content')
+<script>
+    const forgotpassword = ()=>{
+        Swal.fire({
+            icon: 'info',
+            title: '忘記密碼？',
+            text: "請聯絡總管工作人員，核實身分後，由後台統一做修改，謝謝。",
+            confirmButtonText: '確定'
+        })
+    }
+</script>
 @stop
