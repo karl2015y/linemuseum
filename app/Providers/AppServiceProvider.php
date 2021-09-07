1c13c8f335a9ae6644d60dc9ae988a794215a97c
karl <karl2015y@gmail.com>
2021-09-07 14:03:41 +0000

    改成強制https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Url::forceScheme('https');
-        if($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        \Illuminate\Support\Facades\Url::forceScheme('https');
+        // if($this->app->environment('production')) {
+        //     \Illuminate\Support\Facades\URL::forceScheme('https');
+        // }
     }
 }
